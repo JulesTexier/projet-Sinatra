1a9f64517564151c15863b636f11d0d10e3cbb99
JulesTexier <julestexier@live.fr>
2020-10-19 19:28:31 +0200

    first commit

---
+++ lib/gossip.rb
@@ -0,0 +1,49 @@
+require 'pry'
+require 'csv'
+
+class Gossip
+  attr_accessor :author, :content
+    
+    def initialize(author, content)
+        @author = author
+        @content = content
+    end
+
+    def save
+        CSV.open("db/gossip.csv", "ab") do |csv|
+          csv << [@author, @content]
+        end
+    end
+
+    def self.all
+        all_gossips = []
+        CSV.read("db/gossip.csv").each do |csv_line|
+            all_gossips << Gossip.new(csv_line[0], csv_line[1])
+        end
+        return all_gossips
+    end
+
+    def self.find(id)
+        index_gossip = Gossip.all 
+        return index_gossip[id.to_i - 1]        
+    end
+
+    def self.update(id,author,content)
+      gossips = []
+  
+      CSV.read("db/gossip.csv").each_with_index do |row, index|
+        if id.to_i == (index + 1)    # i 
+          gossips << [author, content]
+        else
+          gossips << [row[0], row[1]]
+        end
+      end
+  
+      CSV.open("db/gossip.csv", "w") do |csv| 
+        gossips.each do |row|
+          csv << row
+        end
+      end
+    end     
+end
+
