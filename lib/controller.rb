18a517df4e2447db6578f5ef861284f9acc13990
JulesTexier <julestexier@live.fr>
2020-10-20 09:10:09 +0200

    last one

--- lib/controller.rb
+++ lib/controller.rb
@@ -8,24 +8,28 @@ class ApplicationController < Sinatra::Base
     erb :index, locals: {gossips: Gossip.all}
   end
 
+#envoie vers la route de la page de création de Gossipe
   get '/gossips/new/' do
     erb :new_gossip
   end
   
+#envoie vers la route de la page du Gossipe N°...
   get '/gossips/:id' do
-   # "Voici le numéro du potin que tu veux : #{params['id']}!"
       erb :show, locals: {gossips: Gossip.find(params['id'])}
   end
 
+#récupère les infos du formulaire de création de Gossip
   post '/gossips/new/' do
     Gossip.new(params["gossip_author"], params["gossip_content"]).save
     redirect '/'
   end
 
+#envoie vers la route de la page d'edition du gossip...
   get '/gossips/:id/edit/' do
     erb :edit
   end
 
+#récupère les infos du formulaire d'édition du Gossip
   post '/gossips/:id/edit/' do
     Gossip.update(params['id'], params["edit_author"], params["edit_content"])
     redirect '/'
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -4,17 +4,20 @@ require 'csv'
 class Gossip
   attr_accessor :author, :content
     
+  #j'initialise mes variables
     def initialize(author, content)
         @author = author
         @content = content
     end
 
+    #permet d'enregistrer un nouveau gossip
     def save
         CSV.open("db/gossip.csv", "ab") do |csv|
           csv << [@author, @content]
         end
     end
 
+    #permet d'afficher tous les  Gossip
     def self.all
         all_gossips = []
         CSV.read("db/gossip.csv").each do |csv_line|
@@ -23,16 +26,18 @@ class Gossip
         return all_gossips
     end
 
+    #permet d'enregistrer un nouveau gossip
     def self.find(id)
         index_gossip = Gossip.all 
         return index_gossip[id.to_i - 1]        
     end
 
+    #permet de mettre à jour un gossip avec une variable supp (id)
     def self.update(id,author,content)
       gossips = []
   
       CSV.read("db/gossip.csv").each_with_index do |row, index|
-        if id.to_i == (index + 1)    # i 
+        if id.to_i == (index + 1) 
           gossips << [author, content]
         else
           gossips << [row[0], row[1]]
